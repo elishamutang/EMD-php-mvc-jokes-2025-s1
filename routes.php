bd5b66e81c0a3f9cfa8ff36e65b5030d729495fa
elishamutang <elishamutang@outlook.com>
2025-04-13 15:55:57 +0800

    wip(routes): Added new routes for editing user details.

--- routes.php
+++ routes.php
@@ -17,6 +17,7 @@
 $router->get('/', 'HomeController@index');
 
 $router->get('/dashboard', 'HomeController@dashboard');
+$router->get('/edit', 'HomeController@edit');
 
 $router->get('/auth/register', 'UserController@create', ['guest']);
 $router->get('/auth/login', 'UserController@login', ['guest']);
@@ -24,6 +25,7 @@ $router->get('/auth/login', 'UserController@login', ['guest']);
 $router->post('/auth/register', 'UserController@store', ['guest']);
 $router->post('/auth/logout', 'UserController@logout', ['auth']);
 $router->post('/auth/login', 'UserController@authenticate', ['guest']);
+$router->post('/edit', 'UserController@update');
 
 /**
  * Example Routes for a feature (Feature)
