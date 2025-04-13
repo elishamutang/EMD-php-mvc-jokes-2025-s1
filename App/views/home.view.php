b6505c6d47d633597ef39992f504fa95817f2e01
elishamutang <elishamutang@outlook.com>
2025-04-13 19:44:23 +0800

    wip(home): Updated home view to include 'New Joke' button to display joke.

--- App/views/home.view.php
+++ App/views/home.view.php
@@ -4,12 +4,18 @@
  *
  * Filename:        home.view.php
  * Location:        /App/views
- * Project:         XXX-SaaS-Vanilla-MVC-YYYY-SN
- * Date Created:    23/08/2024
+ * Project:         EMD-php-mvc-jokes-2025-s1
+ * Date Created:    03/04/2025
  *
- * Author:          Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
+ * Author:          Elisha Mutang Daneil <20145565@tafe.wa.edu.au>
  *
  */
+
+/*
+ * Decode joke body stored in database and format it to have a <br> tag if there is more than 1 sentence.
+ */
+$joke_body = html_entity_decode($joke->body);
+$joke_body = str_replace(['<p>', '</p>'], ['', '<br>'], $joke_body);
 
 loadPartial('header');
 loadPartial('navigation');
@@ -19,17 +25,41 @@ loadPartial('navigation');
 <main class="container mx-auto bg-zinc-50 py-8 px-4 shadow shadow-black/25 rounded-lg">
     <article  class="grid grid-cols-1">
         <header class="bg-zinc-700 text-zinc-200 -mx-4 -mt-8 mb-4 p-8 text-2xl font-bold rounded-t-lg">
-            <h1>Vanilla PHP MVC Demo</h1>
+            <h1>HOMEPAGE</h1>
         </header>
 
-        <section class="my-4 p-4  gap-8 justify-start">
-            <p class="text-3xl font-light">Welcome to the XXX SaaS Vanilla MVC YYYY/SN Template App</p>
-            <p>This is a starter template for simple vanilla mvc based applications.</p>
-            <p>Its primary purpose is as a <strong>teaching tool</strong> and is provided with no guarantees for security and production readiness.</p>
+        <section class="my-4 p-4 justify-start">
+            <p class="text-4xl font-bold">Welcome to Jokeify!</p>
+            <p>Created by Elisha Daneil as part of the EMD-PHP-MVC-Jokes-2025-S1 project.</p>
+        </section>
+    </article>
+
+    <article  class="grid grid-cols-1">
+        <section class="p-4 justify-start">
+            <p class="text-2xl font-bold">Push button below for new joke!</p>
+            <form method="GET" action="/">
+                <button type="submit" class="px-4 py-4 btn-secondary cursor-pointer">New Joke</button>
+            </form>
         </section>
     </article>
 
     <article class="grid grid-cols-2 ">
+
+        <section class="m-4 bg-zinc-200 text-zinc-700 p-8 rounded-lg shadow">
+            <dl class="flex flex-col gap-2">
+
+                <dt class="text-lg font-semibold">Featured Joke</dt>
+                <dd class="ml-4 flex items-start">
+                    <i class="hover:text-black fa-regular fa-face-smile-wink inline-block mr-2 text-sm"></i>
+                    <p class="hover:text-black"><?= $joke->title ?></p>
+                </dd>
+                <dd class="ml-4 flex items-start">
+                    <i class="hover:text-black fa-regular fa-face-smile-wink inline-block mr-2 text-sm"></i>
+                    <?= $joke_body ?>
+                </dd>
+            </dl>
+        </section>
+
         <section class="m-4 bg-zinc-200 text-zinc-700 p-8 rounded-lg shadow">
             <dl class="flex flex-col">
                 <dt class="text-lg font-semibold ">Tutorial:</dt>
@@ -49,22 +79,23 @@ loadPartial('navigation');
                 </dd>
             </dl>
         </section>
-            <section class="m-4 bg-zinc-200 text-zinc-700 p-8 rounded-lg shadow">
-                <dl class="flex flex-col">
+        <section class="m-4 bg-zinc-200 text-zinc-700 p-8 rounded-lg shadow">
+            <dl class="flex flex-col">
                 <dt class="text-lg font-semibold">Source Code:</dt>
                 <dd class="ml-4">
-                    <a href="https://github.com/AdyGCode/XXX-SaaS-Vanilla-MVC-YYYY-SN"
+                    <a href="https://github.com/elishamutang/EMD-php-mvc-jokes-2025-s1" target="_blank"
                        class="hover:text-black">
                         <i class="fa fa-code inline-block mr-2 text-sm"></i>
-                        https://github.com/AdyGCode/XXX-SaaS-Vanilla-MVC-YYYY-SN
+                        https://github.com/elishamutang/EMD-php-mvc-jokes-2025-s1
                     </a>
                 </dd>
-                </dl>
-            </section>
+            </dl>
+        </section>
+
         <section class="m-4 bg-zinc-200 text-zinc-700 p-8 rounded-lg shadow">
             <dl class="flex flex-col">
 
-            <dt class="text-lg font-semibold">HelpDesk</dt>
+                <dt class="text-lg font-semibold">HelpDesk</dt>
                 <dd class="ml-4">
                     <a href="https://help.screencraft.net.au"
                        class="hover:text-black">
@@ -88,23 +119,6 @@ loadPartial('navigation');
             </dl>
 
         </section>
-
-        <section class="m-4 bg-zinc-200 text-zinc-700 p-8 rounded-lg shadow">
-            <dl class="flex flex-col">
-
-            <dt class="text-lg font-semibold">Featured Product</dt>
-                <dd class="ml-4">
-                    <a href="https://help.screencraft.net.au"
-                       class="hover:text-black" target="_blank">
-                        <i class="fa fa-home inline-block mr-2 text-sm"></i>
-                        <?= $joke->title ?>
-                    </a>
-                </dd>
-
-            </dl>
-
-        </section>
-
     </article>
 </main>
 
