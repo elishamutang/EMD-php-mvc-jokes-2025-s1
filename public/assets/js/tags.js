eb27b5ff20925591484b86ddb908ed105603373c
elishamutang <elishamutang@outlook.com>
2025-04-19 02:02:50 +0800

    feat(joke-tags): JS script may not be needed and might be removed.

--- public/assets/js/tags.js
+++ public/assets/js/tags.js
@@ -15,35 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Get input tag field.
     const tagInput = document.getElementById('Tags');
 
-    // Detect space key pressed and submit tag on key press.
-    tagInput.addEventListener('keyup', submitOnSpace);
-
 })
 
 function removeOnClick(e) {
     console.log(e.currentTarget.parentElement);
     e.currentTarget.parentElement.remove();
-}
-
-function submitOnSpace(e) {
-    if(e.code === 'Space') {
-        let input = e.target.value.trim().toLowerCase();
-        e.target.value = ''
-        console.log(input)
-
-        if(input !== '') {
-            // Copy an existing tag component and overwrite element details.
-            const newTagComp = document.querySelector('.tag').cloneNode(true);
-
-            newTagComp.id = input
-            newTagComp.querySelector('span').textContent = input
-            newTagComp.querySelector('button').addEventListener('click', removeOnClick, {once: true})
-
-            console.log(newTagComp);
-
-            // Attach to DOM.
-            const tagsContainer = document.getElementById('tags-container');
-            tagsContainer.append(newTagComp);
-        }
-    }
 }
