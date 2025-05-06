d967f4e70cc28ce3fb86be8677240534e1b7cd77
elishamutang <elishamutang@outlook.com>
2025-05-06 13:49:44 +0800

    docs: Updated doc blocks.

--- routes.php
+++ routes.php
@@ -7,10 +7,10 @@
  *
  * Filename:        routes.php
  * Location:        /
- * Project:         XXX-SaaS-Vanilla-MVC-YYYY-SN
- * Date Created:    20/08/2024
+ * Project:         EMD-php-mvc-jokes-2025-s1
+ * Date Created:    13/04/2025
  *
- * Author:          Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
+ * Author:          Elisha Mutang Daneil <20145565@tafe.wa.edu.au>
  *
  */
 
@@ -45,49 +45,3 @@ $router->get('/categories/{id}', 'CategoryController@show', ['auth']);
 $router->post('/categories', 'CategoryController@store', ['auth']);
 $router->put('/categories/{id}', 'CategoryController@update', ['auth']);
 $router->delete('/categories/{id}', 'CategoryController@destroy', ['auth']);
-
-/**
- * Example Routes for a feature (Feature)
- *
- * $router->HTTP_METHOD('PATH', 'CONTROLLER_NAME@METHOD', OPTIONAL_MIDDLEWARE)
- *
- * PATH is based on the pluralised version of the FEATURE_NAME
- * CONTROLLER_NAME is usually in the form "NAME" in Pascal Case with Controller added
- * METHOD is the Controller Class method (function) that is called
- * OPTIONAL_MIDDLEWARE indicates if middleware is used for example to ensure a user is authenticated, or is a guest
- */
-$router->get('/features', 'FeatureController@index');
-$router->get('/features/create', 'FeatureController@create', ['auth']);
-$router->get('/features/edit/{id}', 'FeatureController@edit', ['auth']);
-$router->get('/features/search', 'FeatureController@search');
-$router->get('/features/{id}', 'FeatureController@show');
-
-$router->post('/features', 'FeatureController@store', ['auth']);
-$router->put('/features/{id}', 'FeatureController@update', ['auth']);
-$router->delete('/features/{id}', 'FeatureController@destroy', ['auth']);
-
-/** 
- * Example Product Feature Routes 
- */
-$router->get('/products', 'ProductController@index');
-$router->get('/products/create', 'ProductController@create', ['auth']);
-$router->get('/products/edit/{id}', 'ProductController@edit', ['auth']);
-$router->get('/products/search', 'ProductController@search');
-$router->get('/products/{id}', 'ProductController@show');
-
-$router->post('/products', 'ProductController@store', ['auth']);
-$router->put('/products/{id}', 'ProductController@update', ['auth']);
-$router->delete('/products/{id}', 'ProductController@destroy', ['auth']);
-
-/** 
- * Example Colours Feature Routes 
- */
-$router->get('/colours', 'ColourController@index');
-$router->get('/colours/create', 'ColourController@create', ['auth']);
-$router->get('/colours/edit/{id}', 'ColourController@edit', ['auth']);
-$router->get('/colours/search', 'ColourController@search');
-$router->get('/colours/{id}', 'ColourController@show');
-
-$router->post('/colours', 'ColourController@store', ['auth']);
-$router->put('/colours/{id}', 'ColourController@update', ['auth']);
-$router->delete('/colours/{id}', 'ColourController@destroy', ['auth']);
